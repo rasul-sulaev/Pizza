20e5ba6afdacc2c6bb3bcf533fed27fe0da0b6aa
rasul-sulaev <mr.sylaev@mail.ru>
2023-05-14 21:47:23 +0300

    Создла папку data, и перенес туда JSON данные категорий

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,32 +1,5 @@
 import {useState} from "react";
-
-const categories = [
-	{
-		id: 1,
-		name: 'Все'
-	},
-	{
-		id: 2,
-		name: 'Мясные'
-	},
-	{
-		id: 3,
-		name: 'Вегетарианская'
-	},
-	{
-		id: 4,
-		name: 'Гриль'
-	},
-	{
-		id: 5,
-		name: 'Острые'
-	},
-	{
-		id: 6,
-		name: 'Закрытые'
-	}
-];
-
+import categories from "../data/categories.json";
 
 const Categories = () => {
 	const [selectedCategory, setSelectedCategory] = useState(1);
