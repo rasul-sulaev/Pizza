5f521833a7ed3d702bfb58f42e45032144c22176
rasul-sulaev <mr.sylaev@mail.ru>
2023-05-15 23:37:23 +0300

    Внес правки в компонент CardSkeleton

--- src/components/Card/CardSkeleton.jsx
+++ src/components/Card/CardSkeleton.jsx
@@ -13,10 +13,10 @@ const CardSkeleton = () => {
 				foregroundColor="#e6e6e6"
 			>
 				<circle cx="140" cy="124" r="124" />
-				<rect x="0" y="268" rx="6" ry="6" width="280" height="28" />
-				<rect x="0" y="314" rx="6" ry="6" width="280" height="85" />
+				<rect x="0" y="268" rx="8" ry="8" width="280" height="28" />
+				<rect x="0" y="314" rx="10" ry="10" width="280" height="85" />
 				<rect x="155" y="417" rx="25" ry="25" width="125" height="44" />
-				<rect x="0" y="417" rx="6" ry="6" width="120" height="44" />
+				<rect x="0" y="417" rx="8" ry="8" width="120" height="44" />
 			</ContentLoader>
 		</div>
 	)
