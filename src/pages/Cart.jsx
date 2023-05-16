ce29e7270f1e8375df14b2b2f8c2e52e9206479c
rasul-sulaev <mr.sylaev@mail.ru>
2023-05-16 03:36:54 +0300

    Сверстал состоение Корзины, когда в нем пусто

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,17 @@
+import ContentInfo from "../components/ContentInfo";
+
 const Cart = () => {
 	return (
-		<section>
-			Cart
+		<section className="cart">
+			<div className="cart-empty">
+				<ContentInfo
+					title={'Корзина пустая 😕'}
+					description={<>Вероятней всего, вы не заказывали ещё пиццу. <br/>
+						Для того, чтобы заказать пиццу, перейди на главную страницу.</>}
+				/>
+				<img className="cart-empty__img" src="./img/girl-with-basket.svg" width={300} height={255} alt="Женщина с корзиной"/>
+				<button className="cart-empty__btn btn btn_black">Вернуться назад</button>
+			</div>
 		</section>
 	)
 }
